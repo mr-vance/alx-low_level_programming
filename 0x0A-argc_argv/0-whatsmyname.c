251d3f95ce470922e14b7a065cef598b042fa845
mr-vance <vansmuchongo@gmail.com>
2023-06-11 21:44:24 +0200

    prev argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,6 @@
-#include "main.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    print_program_name(argv[0]);
+    printf("%s\n", argv[0]);
     return 0;
-}
-
-void print_program_name(char *name) {
-    printf("%s\n", name);
 }
