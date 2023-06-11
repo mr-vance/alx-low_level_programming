206d4ebf2fc49030a7c38b6d001ecf4521ca1e7f
mr-vance <vansmuchongo@gmail.com>
2023-06-11 21:51:49 +0200

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,26 @@
 #include "main.h"
 
-int main(int argc, char *argv[]) {
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
     /* Suppress unused parameter warning */
     (void)argc;
+
     print_program_name(argv[0]);
     return 0;
 }
 
-void print_program_name(char *name) {
+/**
+ * print_program_name - Prints the name of the program
+ * @name: The name of the program
+ */
+void print_program_name(char *name)
+{
     printf("%s\n", name);
 }
