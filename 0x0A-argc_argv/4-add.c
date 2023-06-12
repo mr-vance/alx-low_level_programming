db28e1116f9a877f29db0a6505e020885de9ee8d
mr-vance <vansmuchongo@gmail.com>
2023-06-12 02:05:41 +0200

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,81 @@
+#include "main.h"
+
+/**
+ * is_digit - Checks if a character is a digit
+ * @c: The character to check
+ *
+ * Return: 1 if the character is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * is_valid_number - Checks if a string is a valid number
+ * @str: The string to check
+ *
+ * Return: 1 if the string is a valid number, 0 otherwise
+ */
+int is_valid_number(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (!is_digit(str[i]))
+			return (0);
+		i++;
+	}
+
+	return (1);
+}
+
+/**
+ * sum_positive_numbers - Sums the positive numbers passed as arguments
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line arguments
+ *
+ * Return: The sum of the positive numbers, or 0 if no numbers are passed,
+ *         or 1 if there is an invalid number
+ */
+int sum_positive_numbers(int __attribute__((unused)) argc, char *argv[])
+{
+	int i, num, sum = 0;
+
+	if (argc <= 1)
+		return (0);
+
+	for (i = 1; i < argc; i++)
+	{
+		if (is_valid_number(argv[i]))
+		{
+			num = atoi(argv[i]);
+			if (num > 0)
+				sum += num;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	return (sum);
+}
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line arguments
+ *
+ * Return: 0 if successful, 1 otherwise
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int result = sum_positive_numbers(argc, argv);
+
+	printf("%d\n", result);
+
+	return (result == 1 ? 1 : 0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,10 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void print_program_name(char *name);
 int count_arguments(char **argv);
 void print_arguments(char **argv);
 int multiply(int num1, int num2);
+int is_digit(char c);
+int is_valid_number(char *str);
+int sum_positive_numbers(int argc, char *argv[]);
 
 #endif /* MAIN_H */
