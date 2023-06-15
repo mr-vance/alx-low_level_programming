ba041be9a9a146405b777672928b59ec719c0b8c
mr-vance <vansmuchongo@gmail.com>
2023-06-16 00:21:07 +0200

    question 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,13 +11,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	char *ptr;
 
+	ptr = malloc(b);
 	if (ptr == NULL)
-	{
-		printf("Error: malloc failed\n");
 		exit(98);
-	}
 
 	return (ptr);
+
 }
